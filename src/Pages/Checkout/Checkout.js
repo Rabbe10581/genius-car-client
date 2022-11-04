0f70767e822ad8c0e7c2e20c4a53f1b0d3502360
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-04 08:48:26 +0600

    Delete and Update Orders with a protected route added

---
+++ src/Pages/Checkout/Checkout.js
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+
+const Checkout = () => {
+    const service = useLoaderData();
+    const { _id, title, price } = service;
+    const { user } = useContext(AuthContext);
+
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = `${form.firstName.value} ${form.lastName.value}`;
+        const email = user?.email || 'unregistered';
+        const phone = form.phone.value;
+        const message = form.message.value;
+
+        const order = {
+            service: _id,
+            serviceName: title,
+            price,
+            customer: name,
+            email,
+            phone,
+            message
+        }
+        //Validate
+        // if (phone.length < 11) {
+        //     alert('Phone number should be 11 characters or more')
+        // }
+        // else{
+
+        // }
+
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    toast.success('Order placed successfully', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                    form.reset();
+                }
+            })
+            .catch(err => console.error(err));
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handlePlaceOrder}>
+                <h2 className="text-4xl">You are about to order: {title}</h2>
+                <h4 className="text-3xl">{price}</h4>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                    <input name='firstName' type="text" placeholder="First Name" className="input input-bordered w-full" />
+                    <input name='lastName' type="text" placeholder="Last Name" className="input input-bordered w-full" />
+                    <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered w-full" required />
+                    <input name='email' type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
+                </div>
+                <textarea name='message' className="textarea textarea-bordered h-36 w-full" placeholder="Your Message" required></textarea>
+
+                <button className="btn btn-warning text-white w-full">Order Now</button>
+            </form>
+            <ToastContainer></ToastContainer>
+        </div>
+    );
+};
+
+export default Checkout;
